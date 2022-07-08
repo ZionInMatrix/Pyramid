072cb5e6121ad806b7482fd2fe5eb7cba7536037
Rudolf Gasparian <rudolf.gasparian@seznam.cz>
2022-07-08 12:50:28 +0200

    added Tree traversal

---
+++ src/SortAlgorithms/TreeTraversal/Node.java
@@ -0,0 +1,11 @@
+package SortAlgorithms.TreeTraversal;
+
+public class Node {
+    String value;
+    Node left;
+    Node right;
+
+    public Node(String value) {
+        this.value = value;
+    }
+}
